fba4cc8573842d3a64403f036c2f3ed97c14917d
Manvi1807 <md7906@srmist.edu.in>
2026-04-06 09:55:45 +0530

    Initial commit

---
+++ HelloApp(UC7).java
@@ -0,0 +1,15 @@
+public class GreetingApp {
+    public static void main(String[] args) {
+
+        // If no arguments
+        if (args.length == 0) {
+            System.out.println("Hello, World!");
+        } else {
+            // Join names using delimiter ", "
+            String names = String.join(", ", args);
+
+            // Print greeting
+            System.out.println("Hello, " + names + "!");
+        }
+    }
+}
